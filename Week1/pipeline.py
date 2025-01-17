4f3982560b3f2a0efe30c2a11348ccd6b93ac5d1
Shreyank Joshi <sk.joshi267@gmail.com>
2025-01-17 20:07:51 +0000

    Created pipeline in Docker

--- Week1/pipeline.py
+++ Week1/pipeline.py
@@ -1,33 +1,100 @@
-# Import libraries
+# # Import libraries
+import datetime
+from sqlite3 import connect
+from urllib import response
 import pandas as pd
 from sqlalchemy import create_engine
+import requests
+import configparser
+import gzip
+from datetime import datetime
 
-#
-engine = create_engine("postgresql://admin:admin@localhost:5432/nyc_taxi")
+def update_table(data,cfg,connect,table_name):
+    try:
+        print(f'Uploading data to postgres {table_name}')
+        data.to_sql(table_name,con=connect,if_exists="append",index=False)
+        result = True
+    except Exception as ex:
+        print(f'Error with DB Insertion: {ex}')
+        result = False
+    finally:
+        return result
 
-#
-connect = engine.connect()
+def create_table(header_row,cfg):
+    #"postgresql://admin:admin@localhost:5432/nyc_taxi"
+    try:
+        connection_str = f'postgresql://{cfg["user"]}:{cfg["password"]}@{cfg["host"]}:{cfg["port"]}/{cfg["db"]}'
+        engine = create_engine(connection_str)
+        connect = engine.connect()
+        table_name = cfg["url"][-30:].replace(".csv.gz","")
+        print(f'Creating table with {table_name}')
+        header_row.to_sql(table_name,con=connect,if_exists="replace",index=False)
+        result = True
+    except Exception as ex:
+        print(f'Error with DB Operations: {ex}')
+        result = False
+    finally:
+        return result,connect,table_name
 
-#
-df_itr = pd.read_csv(r"yellow_tripdata_2019-01.csv",chunksize=100000,iterator=True)
+def process_data(file,cfg):
+    print(f'Processing Data with {file}')
+    with gzip.open(file,"rt") as file_name:
+        data_itr = pd.read_csv(file_name,chunksize=100000,iterator=True)
+        for idx,data in enumerate(data_itr):
+            if idx == 0:
+                response,connect,table_name = create_table(data.head(0),cfg)
+                if not response: 
+                    break
+            
+            data["tpep_pickup_datetime"] = pd.to_datetime(data["tpep_pickup_datetime"])
+            data["tpep_dropoff_datetime"] = pd.to_datetime(data["tpep_dropoff_datetime"])
+            print(f'Uploading records: {len(data)}')
+            result = update_table(data,cfg,connect,table_name)
+            if not result:
+                break
 
-#
-data = next(df_itr)
+def download_data(url):
+    try:
+        file_name = "data.csv.gz"
+        response = requests.get(url)
+        if response.status_code == 200:
+            with open(file_name,"wb") as file:
+                file.write(response.content)
+    except Exception as ex:
+        print(f'Exception occured" {ex}')
+        file_name = None
+    finally:
+        return file_name
 
-#
-data.info()
+def set_params():
+    config = configparser.ConfigParser()
+    config.read("config.ini")
+    host = config["postgres-params"]["docker_container"]
+    user = config["postgres-params"]["postgres_user"]
+    password = config["postgres-params"]["postgres_pass"]
+    database = config["postgres-params"]["postgres_db"]
+    port = config["postgres-params"]["postgres_port"]
+    url = config["postgres-params"]["download_url"]
+    
+    print(f'Parameters read from config.ini')
+    return {
+        "host":host,
+        "user":user,
+        "password":password,
+        "port":port,
+        "db":database,
+        "url":url
+        }
+    
+if __name__ == "__main__":
+    cfg = set_params()
+    
+    if len(cfg["url"]):
+        print(f'Downloading file from {cfg["url"]}')
+        file  = download_data(cfg["url"])
+        result = process_data(file,cfg)
+    else:
+        print(f'Unable to download data. Invalid URL')
 
-#
-data["tpep_pickup_datetime"] = pd.to_datetime(data["tpep_pickup_datetime"])
-data["tpep_dropoff_datetime"] = pd.to_datetime(data["tpep_dropoff_datetime"])
-
-#
-print(pd.io.sql.get_schema(data,name="yellow_tripdata_2019_01",con=engine))
-
-#
-data.head(0).to_sql("yellow_tripdata_2019-01",con=connect,if_exists="replace",index=False)
-
-#
-data.to_sql("yellow_tripdata_2019-01",con=connect,if_exists="append",index=False)
 
 
